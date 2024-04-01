13683b8644f1ed57e7cc14f712f939f96f77a99d
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-04-02 01:43:29 +0530

    header component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component'; // Importing TodosComponent
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodosComponent, FormsModule],
+  imports: [RouterOutlet, TodosComponent, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

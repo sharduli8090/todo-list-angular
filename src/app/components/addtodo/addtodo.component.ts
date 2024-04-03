4831afc63f7de367c0662081fdabcb2548f6257a
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-04-03 23:54:07 +0530

    styled addtodo

--- src/app/components/addtodo/addtodo.component.ts
+++ src/app/components/addtodo/addtodo.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Todos } from '../../Todos';
 import { TodoService } from '../../todo.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-addtodo',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './addtodo.component.html',
   styleUrl: './addtodo.component.css',
 })
@@ -16,7 +17,9 @@ export class AddtodoComponent implements OnInit {
   ngOnInit(): void {}
   public title: string = ''; // Title of the todo
   public description: string = ''; // Description of the todo
-  public error: string = ''; // Error message
+  public error: boolean = false; // Error message
+  public input: string =
+    'p-2 border-2 border-gray-300 rounded md:w-3/4 lg:w-4/5 shadow-gray-100 shadow-lg '; // Input field
   onSubmit() {
     if (this.title !== '' || this.description !== '') {
       console.log(this.title, this.description);
@@ -27,9 +30,11 @@ export class AddtodoComponent implements OnInit {
         completed: false,
       };
       this.todoService.addTodoMain(todo);
-      this.error = '';
+      this.error = false;
+      alert('Todo Added Successfully!');
     } else {
-      this.error = 'Please fill out all the fields.';
+      console.log(this.error);
+      this.error = true;
     }
     this.title = '';
     this.description = '';
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,13 +8,14 @@ import { RouterLink, RouterModule } from '@angular/router';
   imports: [NgClass, RouterModule, RouterLink],
   templateUrl: './header.component.html',
   styles: `.opacity-custom {
-    background-color: rgba(99,9,9, 0.1); 
+    background-color: rgba(299,299,299, 0.3); 
   }`,
 })
 export class HeaderComponent {
   button1Active: boolean = false;
   button2Active: boolean = true;
-  btn_route_common: string = ' px-10 py-2 rounded  text-md';
+  btn_route_common: string =
+    'px-3 py-1 md:px-9 md:py-2 lg:px-10 lg:py-2 rounded text-xs md:text-md lg:text-lg';
 
   toggleButton1(): void {
     if (!this.button1Active) {
